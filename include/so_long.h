aefaa6d47324543cce4f82d021e741e9910a737f
Your Name <nkeyani-@student.42barcelona.com>
2023-07-21 14:03:56 +0200

    window init

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 23:22:31 by bifrost           #+#    #+#             */
-/*   Updated: 2023/07/21 11:59:25 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/07/21 14:02:38 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,42 @@
 
 typedef struct s_map
 {
-	uint16_t x;
-	uint16_t y;
+	uint16_t	x;
+	uint16_t	y;
 }	t_map;
 
 typedef struct s_entity
 {
-	uint16_t x;
-	uint16_t y;
+	uint16_t	x;
+	uint16_t	y;
+	//struct		*s_map;
 }	t_entity;
-
 
 
 typedef struct s_player
 {
-	uint16_t x;
-	uint16_t y;
+	uint16_t	x;
+	uint16_t	y;
+	//struct		*s_map;
 }	t_player;
+
+// mlx main loop
+void	solong_mlx_init();
+
+//map
+void	solong_map_load();
+void	solong_map_create();
+int		solong_map_destroy(void *ptr);
+
+
+//player
+void	solong_player_load();	
+void	solong_player_create();	
+void	solong_player_destroy();
+
+//entity 
+void	solong_entity_load();	
+void	solong_entity_create();	
+void	solong_entity_destroy();
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,28 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 23:21:56 by bifrost           #+#    #+#             */
-/*   Updated: 2023/07/21 11:52:26 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/07/21 14:03:25 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+
+int	solong_map_destroy(void *ptr)
+{
+	mlx_destroy_window (ptr, ptr);
+	exit(EXIT_SUCCESS);
+}
+
 int	main(void)
 {
+	void *init = mlx_init ();
 	ft_printf("Soooo long");
+	void *load = mlx_new_window(init, 800, 400, "So long");
+	mlx_loop(init);
+	mlx_clear_window(init, load);
+	mlx_destroy_window(init, load);
+	mlx_hook(init, 17, 1L<<0, &solong_map_destroy, &load);	
+
 	return 0;
 }
