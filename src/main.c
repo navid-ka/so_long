e2317db3e001909629623db97049e4b4f1104974
Your Name <nkeyani-@student.42barcelona.com>
2023-07-21 20:28:14 +0200

    Drawing things

--- src/main.c
+++ src/main.c
@@ -6,26 +6,51 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 23:21:56 by bifrost           #+#    #+#             */
-/*   Updated: 2023/07/21 14:47:41 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/07/21 20:18:53 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
 
-int	solong_map_destroy(void *ptr)
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	mlx_destroy_window (ptr, ptr);
-	exit(EXIT_SUCCESS);
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
+#define WIDTH 1920
+#define HEIGHT 1080
 
 int	main(void)
 {
-	void *init = mlx_init ();
-	ft_printf("Soooo long");
-	void *load = mlx_new_window(init, 800, 400, "So long");
-	mlx_hook(init, 17, 1L<<0, &solong_map_destroy, &load);
-	mlx_loop(init);
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
 
-	return 0;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "Hello world!");
+	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	for (size_t i = 0; i < 100; i++)
+		my_mlx_pixel_put(&img, i, 100, 0x0000FF00);
+	for (size_t j = 0; j < 100; j++)
+		my_mlx_pixel_put(&img, j + 1, j, 0x00FF0000);
+	for (size_t k = 200; k < WIDTH/2; k++)
+		my_mlx_pixel_put(&img, WIDTH/2, k, 0x00FF0000);
+	for (size_t l = 200; l < HEIGHT/2; l++)
+		my_mlx_pixel_put(&img, l + HEIGHT/2, HEIGHT/2, 0x00FF0000);
+	mlx_string_put(mlx, mlx_win, WIDTH/2, HEIGHT/2, 0x00FF0000, "SO LONG");
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
