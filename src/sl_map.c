f64ff54bfde81a8c4336dc8caf6fe6c8555bec1b
Your Name <nkeyani-@student.42barcelona.com>
2023-08-01 17:34:54 +0200

    emergency push

--- src/sl_map.c
+++ src/sl_map.c
@@ -6,7 +6,7 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 13:09:43 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/07/31 15:24:45 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/08/01 15:23:27 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	sl_map_read(int fd, t_game *g)
 	char	*line;
 	char	*tmp_line;
 	char	*all_lines;
-	(void)g;
 
 	line = ft_strdup("");
 	all_lines = ft_strdup("");
@@ -31,10 +30,23 @@ void	sl_map_read(int fd, t_game *g)
 	}
 	//ft_printf("%s,", all_lines);
 	g->map = ft_split(all_lines, '\n');
-	for (size_t i = 0; g->map[i]; i++) {
+	for (size_t i = 0; g->map[i]; i++) { 
         ft_printf("%s\n", g->map[i]);
     }
 	free(all_lines);
+}
+
+void	sl_map_parser(t_game *g)
+{
+	
+	t_game *c;
+	
+	c = g;
+	for (size_t i = 0; c->map[i]; i++) { 
+        ft_printf("%s\n", c->map[i]);
+    }
+		ft_printf("%p\n", c);
+		ft_printf("%p\n", g);
 }
 
 /*void	sl_map_create(int fd, t_game *g)
@@ -54,6 +66,7 @@ void	sl_map_init(char **argv, t_game *g)
 	fd = open(argv[1], O_RDONLY);
 	ft_printf("fd open %d\n", fd);
 	sl_map_read(fd, g);
+	sl_map_parser(g);
 	close(fd);
 	//g->map = ft_split(g->map, '\n');
 }
