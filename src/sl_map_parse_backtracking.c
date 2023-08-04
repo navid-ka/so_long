d19571afefe00558f8abed72a333ec23efe15982
Your Name <nkeyani-@student.42barcelona.com>
2023-08-04 18:17:37 +0200

    starting backtracking error in mamcpy

---
+++ src/sl_map_parse_backtracking.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sl_map_parse_backtracking.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 17:02:37 by nkeyani-          #+#    #+#             */
+/*   Updated: 2023/08/04 17:42:53 by nkeyani-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/so_long.h"
+
+void	sl_map_parse_backtracking(t_game *g)
+{
+	char **mapcpy;
+	int i;
+
+	i = 0;
+	mapcpy = sl_map_cpy(g);
+	/*for (int i = 0; mapcpy[i] != NULL; i++){
+        ft_printf("cpy\t%s\n", mapcpy[i]);
+    }*/
+	/*while (mapcpy[i] != NULL)
+		ft_printf("cpy\t%s\n", mapcpy[i++]);*/
+}
