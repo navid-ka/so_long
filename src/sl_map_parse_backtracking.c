a222c3a44575254ac540e061791a45494142faae
Your Name <nkeyani-@student.42barcelona.com>
2023-08-06 14:54:27 +0200

    Emergency push

--- src/sl_map_parse_backtracking.c
+++ src/sl_map_parse_backtracking.c
@@ -6,28 +6,24 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 17:02:37 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/08/05 20:18:35 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/08/06 12:43:53 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-/*
-static bool	sl_bt_is_valid(t_game *g)
+
+static bool	sl_bt_is_valid(t_game *g, int i, int j)
 {
-	
+	return (g->mapcpy[i][j] != '1' && (i >= 0) && (j >= 0));
 }
-static bool	sl_bt_is_visited(t_game *g)
-{
-	
-}
-*/
-static void	sl_bt_starting(t_game *g)
+
+void	sl_bt_starting(t_game *g)
 {
 	int	i;
 	int	j;
 
-	(0 || i = 0 || j = 0 || g->c = 0 || g->p_start_x = 0 || g->p_start_y = 0);
+	(0 || (i = 0) || (j = 0) || (g->c = 0) || (g->p_start_x = 0) || (g->p_start_y = 0));
 	while (g->mapcpy[i])
 	{
 		while (g->mapcpy[i][j])
@@ -46,8 +42,13 @@ static void	sl_bt_starting(t_game *g)
 }
 
 /*
+void	sl_map_backtrack(t_game *g)
+{
+	
+}
+
 void	sl_map_backtracking(t_game *g)
 {
 	
 }
-*/
+*/ 
